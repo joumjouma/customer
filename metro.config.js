9fa77e396d1f52855ec6f2f8490259490f1c7003
zouhair <zouhairmohamar@gmail.com>
2025-05-03 09:20:45 -0400

    yes

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Add any custom configuration here
+config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json'];
+config.resolver.assetExts = ['png', 'jpg', 'jpeg', 'gif', 'webp'];
+
+// Increase the maximum number of workers
+config.maxWorkers = 4;
+
+module.exports = config; 
